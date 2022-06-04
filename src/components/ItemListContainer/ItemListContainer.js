f4069e75abf64d7d1955b8bb62d1f447461ed713
Antonio Dios <antonidb182@outlook.com>
2022-06-04 00:42:00 -0500

    agregando css card

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,26 @@
 import '../../styles/main.css';
 import {getCatalogo} from '../../asyncmocj'
+import {useState, useEffect} from 'react'
 
 const ItemListContainer = (props) => {
+
+    const [catalogo, setCatalogo] = useState([])
+
+    useEffect(() =>{
+        getCatalogo().then(respuesta=>{
+            setCatalogo(respuesta)
+        })
+    })
+
     return(
-        <div className="divSaludo"><h1>{props.saludo}&nbsp;{props.nombre}</h1></div>
+        <div>
+            <div className="divSaludo"><h1>{props.saludo}&nbsp;{props.nombre}</h1></div>
+            <div className="card">
+
+            </div>
+        </div>
+        
+
     )
 }
 export default ItemListContainer;
