eec0367c6b682bfe604c4fcd5d87687db9914d08
Antonio Dios <antonidb182@outlook.com>
2022-06-19 00:53:33 -0500

    aviso producto agregado

--- src/components/ItemDetail/itemDetail.js
+++ src/components/ItemDetail/itemDetail.js
@@ -1,18 +1,26 @@
 import Contador from '../Contador/Contador'
 import CartContexto from '../../context/CartContext/CartContext'
 import {Link} from 'react-router-dom'
+import {useEffect} from 'react'
 
 import { createContext, useState, useContext } from 'react'
 
 const ItemDetail = ({Id,Nombre,Precio,Descripcion,Imagen,Categoria, Stock}) =>{
 
-    const {agregaProducto,eliminProducto} = useContext(CartContexto)
+    const {agregaProducto} = useContext(CartContexto)
     const [cantidadAgregada, setCantidadAgregada] = useState(0)
+    const [avisoAgregado, setAvisoAgregado] = useState('')
+
+    useEffect(() =>{
+        if(cantidadAgregada !== 0){
+            setAvisoAgregado('Producto Agregado')
+        }
+    },[cantidadAgregada])
 
     const controlAgregaProducto = (cantidad) => {
         agregaProducto({Id, Nombre, Precio, cantidad})
         setCantidadAgregada(cantidad)
-        console.log(cantidad)
+        
     }
 
     return(
@@ -27,8 +35,10 @@ const ItemDetail = ({Id,Nombre,Precio,Descripcion,Imagen,Categoria, Stock}) =>{
                     <div className="detalle-tit-sec">{Nombre}</div>
                     <div className="detalle-precio">S/.&nbsp;{Precio}</div>
                     <div className="detalle-tit-desc">{Descripcion}</div>
+                    
+                    {cantidadAgregada === 0 ? null : <h3 className="detalle-prod-agrega">{avisoAgregado}</h3>}
                     {cantidadAgregada === 0 ? <Contador agrega={controlAgregaProducto} stock={Stock}/> : <Link to='/Cart' className="btnTerminarCompra">Terminar Compra</Link>}
-                    {cantidadAgregada === 0 ? null : <button onClick={() =>eliminProducto(Id)} className="btnEliminarCompra">Eliminar Compra</button>}
+                    
                 </div>
             </div>
             
